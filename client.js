45d7242e0e729aa81964d450bebb0d6505675244
jmbielejeski <jmbielejeski@gmail.com>
2021-01-28 13:55:19 -0700

    added .val to grab value

--- client.js
+++ client.js
@@ -24,7 +24,19 @@ function onReady() {
 
   //grab button then write on click calls this functions
   $('#submitBtn').on('click', whenIAddAHarmonica);
+  $('.deleteBtn').on('click', deleteMe);
+
+  //tie in delete button to an element that is rendered already.
+  //pass a parent element then the element you want to use then the function
+  //document is the parent of all
+  //event delegation - descendant selectors
+  $(document).on('click', '.deleteBtn', deleteMe);
   //can pass in one function on click event
+}
+
+function deleteMe() {
+  console.log('Delete me');
+  $('.harmonicaItem').remove();
 }
 
 //click event
@@ -34,7 +46,15 @@ function whenIAddAHarmonica() {
 
   //grab data from input field
   let brand = $('#brand').val();
-  console.log(brand);
+  console.log('brand is', brand);
+
+  //render this to DOM on a list
+  $('#harmonicas').append(`
+  <li class ="harmonicaItem">
+    Brand is: ${brand}
+    <button class="deleteBtn">Delete Me</button>
+  </li>
+  `);
 }
 
 //DOM - document object model - your HTML
